925e6c5136af69b821048174d052c4353841f91c
Rune Nielsen <runenielsen@runbox.com>
2022-08-09 20:43:01 +0200

    improve error logging

--- src/OpenFTTH.AddressSearchIndexer/AddressSearchIndexerHost.cs
+++ src/OpenFTTH.AddressSearchIndexer/AddressSearchIndexerHost.cs
@@ -27,44 +27,56 @@ internal sealed class AddressSearchIndexerHost : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        _logger.LogInformation($"Starting {nameof(AddressSearchIndexerHost)}.");
-
-        _logger.LogInformation("Starting initial cleanup.");
-        await _addressSearchIndexer.InitialCleanup().ConfigureAwait(false);
-
-        _logger.LogInformation("Starting dehydration.");
-        await _eventStore.DehydrateProjectionsAsync(stoppingToken).ConfigureAwait(false);
-
-        _logger.LogInformation(
-            "Memory after dehydration {MibiBytes}.",
-            Process.GetCurrentProcess().PrivateMemorySize64 / 1024 / 1024);
-
-        var projection = _eventStore.Projections.Get<AddressSearchIndexProjection>();
-
-        await _addressSearchIndexer.Index(projection).ConfigureAwait(false);
-
-        _logger.LogInformation(
-            "Memory after address indexing {MibiBytes}.",
-            Process.GetCurrentProcess().PrivateMemorySize64 / 1024 / 1024);
-
-        while (!stoppingToken.IsCancellationRequested)
+        try
         {
-            await Task.Delay(_catchUpTimeMs, stoppingToken).ConfigureAwait(false);
+            _logger.LogInformation($"Starting {nameof(AddressSearchIndexerHost)}.");
 
-            _logger.LogInformation("Checking for new events.");
-            var changes = await _eventStore
-                .CatchUpAsync(stoppingToken)
+            _logger.LogInformation("Starting initial cleanup.");
+            await _addressSearchIndexer.InitialCleanup().ConfigureAwait(false);
+
+            _logger.LogInformation("Starting dehydration.");
+            await _eventStore
+                .DehydrateProjectionsAsync(stoppingToken)
                 .ConfigureAwait(false);
 
-            if (changes > 0)
+            _logger.LogInformation(
+                "Memory after dehydration {MibiBytes}.",
+                Process.GetCurrentProcess().PrivateMemorySize64 / 1024 / 1024);
+
+            var projection = _eventStore.Projections.Get<AddressSearchIndexProjection>();
+
+            await _addressSearchIndexer.Index(projection).ConfigureAwait(false);
+
+            _logger.LogInformation(
+                "Memory after address indexing {MibiBytes}.",
+                Process.GetCurrentProcess().PrivateMemorySize64 / 1024 / 1024);
+
+            while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("{Count} changes so we do import.", changes);
-                await _addressSearchIndexer.Index(projection).ConfigureAwait(false);
+                await Task.Delay(_catchUpTimeMs, stoppingToken).ConfigureAwait(false);
+
+                _logger.LogInformation("Checking for new events.");
+                var changes = await _eventStore
+                    .CatchUpAsync(stoppingToken)
+                    .ConfigureAwait(false);
+
+                if (changes > 0)
+                {
+                    _logger.LogInformation("{Count} changes so we do import.", changes);
+                    await _addressSearchIndexer
+                        .Index(projection)
+                        .ConfigureAwait(false);
+                }
+                else
+                {
+                    _logger.LogInformation("No changes since last run.");
+                }
             }
-            else
-            {
-                _logger.LogInformation("No changes since last run.");
-            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("{Exception}", ex);
+            throw;
         }
     }
 }
